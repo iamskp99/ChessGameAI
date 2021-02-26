6f501620e9121a7a4b3643a58b647f4a11a501b1
Shashank Kumar Pandey <42648568+iamskp99@users.noreply.github.com>
2021-02-27 02:15:14 +0530

    Added Rook and Updated Pawn.

--- ChessGameAI/ChessGameHeader.h
+++ ChessGameAI/ChessGameHeader.h
@@ -76,16 +76,214 @@ public:
         if(abs_diffof_x > 1) {
             return false;
         }
-        if (start_y + 1 != end_y) {
-            return false;
+          
+        if (this->isFirstMove() == true) {
+            if (abs_diffof_x == 0) {
+                //We will have to define isEmpty() for the spot.
+                if (end.isEmpty() == false) {
+                    return false;
+                }
+                else {
+                    if (this->isWhite() == true) {
+                        if (start_y + 1 != end_y || start_y + 2 != end_y) {
+                            return false;
+                        }
+                    }
+                    else {
+                        if (start_y - 1 != end_y || start_y - 2 != end_y) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else {
+                //Now for the Kill move.
+                if (end.isEmpty() == true) {
+                    return false;
+                }
+                else {
+                    if (this->isWhite() == true) {
+                        if (start_y + 1 != end_y || end.getPiece().isWhite() == true) {
+                            return false;
+                        }
+                    }
+                    else {
+                        if (start_y - 1 != end_y || end.getPiece().isWhite() == false) {
+                            return false;
+                        }
+                    }
+                }
+            }
         }
-        //This needs to be checked.
-        if (end.getPiece() != nullptr && abs_diffof_x == 0) {
-            return false;
+        else {
+            if (abs_diffof_x == 0) {
+                //We will have to define isEmpty() for the spot.
+                if (end.isEmpty() == false) {
+                    return false;
+                }
+                else {
+                    if (this->isWhite() == true) {
+                        if (start_y + 1 != end_y) {
+                            return false;
+                        }
+                    }
+                    else {
+                        if (start_y - 1 != end_y) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else {
+                //Now for the Kill move.
+                if (end.isEmpty() == true) {
+                    return false;
+                }
+                else {
+                    if (this->isWhite() == true) {
+                        if (start_y + 1 != end_y || end.getPiece().isWhite() == true) {
+                            return false;
+                        }
+                    }
+                    else {
+                        if (start_y - 1 != end_y || end.getPiece().isWhite() == false) {
+                            return false;
+                        }
+                    }
+                }
+            }
         }
         return true;
     }
+};
 
+class Rook :public Piece {
+private:
+    //Will have to check what to store in private.
+
+public:
+    Rook(bool white) : Rook(white) {
+
+    }
+    bool canMove(Board board, Spot start, Spot end) {
+        if (end.getPiece().isWhite() == this->isWhite()) {
+            return false;
+        }
+        int start_x = start.getX();
+        int start_y = start.getY();
+        int end_x = end.getX();
+        int end_y = end.getY();
+        if (start_x != end_x && start_y != end_y) {
+            return false;
+        }
+        //Now to check if there is a piece blocking our way or not.
+        int flag = 0;
+        if (start_x != end_x) {
+            if (end_y > start_y) {
+                int i = start_y;
+                while (i < end_y && flag == 0) {
+                    //We will have to define a getBox() method such that we can check if there is a piece or not on that spot.
+                    if (getBox(start_x, i).isEmpty() == false) {
+                        flag = 1;
+                    }
+                    i = i + 1;
+                }
+                if (flag == 1) {
+                    return false;
+                }
+            }
+            else {
+                int i = start_y;
+                while (i > end_y && flag == 0) {
+                    //We will have to define a getBox() method such that we can check if there is a piece or not on that spot.
+                    if (getBox(start_x, i).isEmpty() == false) {
+                        flag = 1;
+                    }
+                    i = i - 1;
+                }
+                if (flag == 1) {
+                    return false;
+                }
+            }
+        }
+        else {
+            if (end_x > start_x) {
+                int i = start_x;
+                while (i < end_x && flag == 0) {
+                    //We will have to define a getBox() method such that we can check if there is a piece or not on that spot.
+                    if (getBox(i, start_y).isEmpty() == false) {
+                        flag = 1;
+                    }
+                    i = i + 1;
+                }
+                if (flag == 1) {
+                    return false;
+                }
+            }
+            else {
+                int i = start_x;
+                while (i > end_x && flag == 0) {
+                    //We will have to define a getBox() method such that we can check if there is a piece or not on that spot.
+                    if (getBox(i, start_y).isEmpty() == false) {
+                        flag = 1;
+                    }
+                    i = i - 1;
+                }
+                if (flag == 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+class Bishop : public Piece {
+public:
+    Bishop(bool white) : Piece(white) {
+
+    }
+    bool canMove(Board board, Spot start, Spot end) {
+        if (end.getPiece().isWhite() == this->isWhite()) {
+            return false;
+        }
+        int start_x = start.getX();
+        int start_y = start.getY();
+        int end_x = end.getX();
+        int end_y = end.getY();
+        int x = abs(start.getX() - end.getX());
+        int y = abs(start.getY() - end.getY());
+        if (x != y) {
+            return false;
+        }
+        else{
+            // check if there is a piece blocking the way
+            int flag = 0;
+            if (start_x < end_x) {
+                if (start_y < end_y) {
+                    int i_x = start_x;
+                    int i_y = start_y;
+                    while (i_x < end_x) {
+                        if (getBox(i_x, i_y).isEmpty() == false) {
+                            flag = 1;
+                        }
+                        i_x += 1;
+                        i_y += 1;
+                    }
+                }
+                else {
+                   //To be added. 
+                }
+            }
+            else {
+                //To be added.
+            }
+            if (flag == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 class Piece {
 private:
